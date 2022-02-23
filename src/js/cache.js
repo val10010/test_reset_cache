3b0db5fae317daec7510f02c51651041f863cc20
v.kiriiak <valentyn.kiriiak@playtech.com>
2022-02-23 13:29:36 +0200

    init

--- src/js/cache.js
+++ src/js/cache.js
@@ -9,7 +9,7 @@ import 'css/imports.scss';
 
 const App = () => (
     <ThemeProvider theme={theme}>
-        cache4
+        cache5
     </ThemeProvider>
 );
 
--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,7 @@ import 'css/imports.scss';
 
 const App = () => (
     <ThemeProvider theme={theme}>
-        without cache4
+        without cache5
     </ThemeProvider>
 );
 
